8e1280f2b17ddb871fa308d9ec50f3248036694f
Richie <rgkirch@gmail.com>
2016-11-05 15:21:53 -0400

    append string length

--- sherlock-anagrams.py
+++ sherlock-anagrams.py
@@ -1,16 +1,17 @@
 from collections import Counter
 
 def hash(string):
-    return sum(["abcdefghijklmnopqrstuvwxyz".index(char) for char in string])
+    return int(str(sum([pow("abcdefghijklmnopqrstuvwxyz".index(char), 2) for char in string])) + str(len(string)))
 
 # takes one string and returns a list of all substrings
 def superString(string):
-    for length in range(len(string))[1:]:
-        for start in range(len(string) - length - 1):
-            yield string[start : start + length]
+    if not string:
+        return []
+    else:
+        return [string[x:] for x in range(len(string))] + list(superString(string[:-1]))
 
 def case(string):
     return sum([x *(x-1)/2 for x in Counter(map(hash,superString(string))).values()])
-            
+
 for testCase in range(int(input())):
-    print(list(superString(input())))
+    print(case(input()))
